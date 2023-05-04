d0bee9a71bc6dff0f3d39d0efb0945cd4933c70b
isalzufari <isalzufari@gmail.com>
2023-05-04 19:29:28 +0700

    implement-cache-service-worker

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,8 @@ const CONFIG = {
   BASE_URL_ANIMAL: 'https://animal.zinu.my.id/api/',
 
   // CACHE
-  CACHE_NAME: 'AnimalApp-V1',
+  // CACHE_NAME: 'AnimalApp-V1',
+  CACHE_NAME: new Date().toISOString(), // Development
 };
 
 export default CONFIG;
--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,41 @@
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+
+// Asset yang akan dicache (app shell)
+const assetsToCache = [
+  './',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './index.html',
+  './favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
 self.addEventListener('install', (event) => {
   console.log('Installing Service Worker ...');
 
   // TODO: Caching App Shell Resource
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Activating Service Worker ...');
 
   // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
 
-  event.respondWith(fetch(event.request));
   // TODO: Add/get fetch request to/from caches
+  // event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
