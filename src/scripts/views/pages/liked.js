95721a4e1a6aeadcf1fe3f33b0216b83849913dc
isalzufari <isalzufari@gmail.com>
2023-05-04 19:42:59 +0700

    show liked from database

--- src/scripts/views/pages/liked.js
+++ src/scripts/views/pages/liked.js
@@ -1,14 +1,35 @@
-import AnimalApiSource from '../../data/animal-api-source';
+import FavoriteAnimalIdb from '../../data/favorite-animal-idb';
+import { createAnimalItemTemplate, createItemEmpty } from '../templates/template-creator';
 
 const Liked = {
   async render() {
     return `
-      <h2>Show Liked Page</h2>
+    <div class="content">
+      <h2 class="content__heading">Your Lovely Animals</h2>
+      <div id="info" class="content__info">
+      
+      </div>
+      <div id="animals" class="animals">
+      
+      </div>
+    </div>
     `;
   },
 
   async afterRender() {
     // Fungsi ini akan dipanggil setelah render()
+    const animals = await FavoriteAnimalIdb.getAllAnimal();
+
+    if (animals.length > 0) {
+      const moviesContainer = document.querySelector('#animals');
+
+      animals.forEach((movie) => {
+        moviesContainer.innerHTML += createAnimalItemTemplate(movie);
+      });
+    } else {
+      const infoContainer = document.querySelector('#info');
+      infoContainer.innerHTML = createItemEmpty();
+    }
   },
 };
 
