4dde00ea69da3f19341dcadbd8de5aacc88c56f2
isalzufari <isalzufari@gmail.com>
2023-05-04 16:14:40 +0700

    configuration-drawer

---
+++ src/scripts/views/app.js
@@ -0,0 +1,24 @@
+import DrawerInitiator from '../utils/drawer-initiator';
+
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {
+    // TODO: initial Drawer
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+
+    // kita bisa menginisiasikan komponen lain bila ada
+  }
+};
+
+export default App;
